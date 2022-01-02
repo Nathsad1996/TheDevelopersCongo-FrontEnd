a3eb4fb982c78b8fcbb03b81e71ae1c9e789f38f
Nathan Sadala <bernynath@gmail.com>
2022-01-02 17:34:25 +0100

    modify project and adding vue 3.2 sugar syntax

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,11 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div>{{ message }}</div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script setup>
+import { ref } from "vue";
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+const message = ref("TheDevelopers Congo");
+
+
 </script>
