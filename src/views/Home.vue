675fb4d4d54c79c9440a6504495e03d056bd3b05
Nathan Sadala <bernynath@gmail.com>
2022-01-22 23:01:14 +0100

    adding mobile-first design in nav

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="grid grid-rows-2 gap-4">
     <nav class="absolute flex justify-between top-0 w-full h-16 border-transparent border-b-4 shadow-lg bg-blue-300">
-      <div class="flex m-3 text-xl cursor-pointer">
+      <div class="flex m-3 text-sm md:text-xl cursor-pointer">
         <span>
           TheDevelopersCongo
         </span>
@@ -12,7 +12,7 @@
         <li class="menu-item">About us</li>
       </ul>
     </nav>
-    <div class="mt-20 grid grid-cols-3 gap-4">
+    <div class="mt-20 grid grid-rows-1 md:grid-cols-3 gap-2 md:gap-4">
       <div class="section-home">This is the grid body</div>
       <div class="section-home">This is the grid body</div>
       <div class="section-home">This is the grid body</div>
